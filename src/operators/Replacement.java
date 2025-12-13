3dc55dbea3c5b3bee61e6d554c691898e2fcd184
nourtarek885 <nourtarek885@gmail.com>
2025-12-13 20:03:50 +0200

    Delete Replacement.java

--- src/operators/Replacement.java
+++
@@ -1,9 +0,0 @@
-package operators;
-
-import java.util.List;
-
-import core.Chromosome;
-
-public interface Replacement {
-    public abstract List<Chromosome> replace(List<Chromosome> parents, List<Chromosome> children);
-}
