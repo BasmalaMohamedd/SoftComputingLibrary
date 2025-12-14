3166371109962ac83f959789c026f37a217a24c7
nourtarek885 <nourtarek885@gmail.com>
2025-12-14 14:59:46 +0200

    LossFunctions

--- src/neuralnetwork/loss/CrossEntropy.java
+++ src/neuralnetwork/loss/CrossEntropy.java
@@ -1,8 +1,21 @@
+//works well with sigmoid binary
+//works well with softmax categorical
+
 package neuralnetwork.loss;
 
 public class CrossEntropy implements LossFunction {
     
-   
-    
-}
+   @Override
+    public double calculateLoss(double expected, double predicted) {
+       return -(expected * Math.log(predicted) + (1 - expected) * Math.log(1- predicted));
+   }
 
+//    public double calculateLoss(double expected, double predicted) {
+//        return expected * Math.log(predicted);
+//    }
+//
+
+    public double derivative(double expected, double predicted) {
+        return predicted - expected;
+    }
+}
