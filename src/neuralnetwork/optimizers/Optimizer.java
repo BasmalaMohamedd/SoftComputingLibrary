b1a29955748dadcf2748c6dc25a91ada45e44b4c
thomasahdy <thomasahdy274@gmail.com>
2025-12-17 00:51:43 +0200

    alsheriff work

--- src/neuralnetwork/optimizers/Optimizer.java
+++ src/neuralnetwork/optimizers/Optimizer.java
@@ -1,7 +1,7 @@
 package neuralnetwork.optimizers;
 
-
 public interface Optimizer {
-    
-}
 
+    void update(double[][] weights, double[][] gradients, double learningRate);
+    void update(double[] biases, double[] gradients, double learningRate);
+}
